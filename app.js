dba59319e20979831a961af0f4fe7b8ebe084731
Damien <damien.jisseau@outlook.fr>
2022-03-18 09:34:54 +0100

    Merging views with backend

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-// Importation du module 'babel-register'
 require('babel-register');
 
 const { connexDb } = require("./config/default.js");
 
 // Exportation du module 'express' permettant de créer un serveur
 const express = require('express');
-
-// Exportation du module 'body-parser' permettant d'utiliser la méthode POST
 const bodyParser = require('body-parser');
-
-// Création d'une instance de express
 const app = express();
 
-app.use(bodyParser.json()); // Permet de parser les données JSON
-app.use(bodyParser.urlencoded({ extended: true })); // Permet de parser les données urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'pug');
 
-// HTTP request, HTTP response, next function
-app.use((req, res, next) => {
-    console.log('request url: ', req.url);
-    next(); // Permet d'exécuter la suite de la requête
+const contactList = [
+    { id: 1, firstname: 'John', lastname: 'Doe', email: 'john.doe@gmail.com', mobile: '0601020304' },
+    { id: 2, firstname: 'Michel', lastname: 'Dupont', email: 'michel.dupont@gmail.com', mobile: '0701020304' }
+] 
+
+app.get('/', (req, res) => {
+    res.render('index', { contactList: contactList });
 })
 
-// Création d'une route GET.
-app.get('/api', function(req, res){
-    res.send('Hello, world!');
+app.get('/login', (req, res) => {
+    res.render('login');
 })
 
 app.post('/add_member', async (req, res) => {
